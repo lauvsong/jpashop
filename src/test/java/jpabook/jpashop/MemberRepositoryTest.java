a319263b59c70f725ee91bef6ae7e1cd709a6a4e
lauvsong <hyehye9355@naver.com>
2022-02-08 03:53:09 +0900

    feat: 도메인 클래스 개발

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,17 +1,15 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
